dd6d444310fb499b7f9f7260d08f58337ddff8d6
xuzc <24253158@qq.com>
2025-08-01 00:07:56 +0800

    feat: websocket长连接

--- main.js
+++ main.js
@@ -2,15 +2,33 @@ import App from './App'
 import Vue from 'vue'
 import io from './commons/js/weapp.socket.io.js'
 Vue.config.productionTip = false
-// 修改为 HTTPS 地址和 443 端口
-Vue.prototype.serverUrl = 'https://www.xiaobei.space' 
-// 修改后
-Vue.prototype.socket = io('wss://www.xiaobei.space', {
-    transports: ['websocket'], // 强制使用 WebSocket 协议
-    timeout: 10000 // 设置连接超时时间（毫秒）
-});
 // Vue.prototype.serverUrl = 'http://172.17.121.229:3000' 
 // Vue.prototype.socket = io('http://172.17.121.229:8002')
+Vue.prototype.serverUrl = 'https://www.xiaobei.space' 
+
+// 明确指定socket.io路径，与Nginx配置匹配
+Vue.prototype.socket = io('wss://www.xiaobei.space', {
+    transports: ['websocket'],
+    timeout: 10000,
+    path: '/socket.io',  // 添加路径配置
+    reconnection: true,  // 开启自动重连
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000
+});
+
+// 添加连接状态监听，便于调试
+Vue.prototype.socket.on('connect', () => {
+    console.log('WebSocket连接成功');
+});
+
+Vue.prototype.socket.on('connect_error', (err) => {
+    console.error('连接错误:', err.message);
+});
+
+Vue.prototype.socket.on('disconnect', (reason) => {
+    console.log('连接断开:', reason);
+});
+
 App.mpType = 'app'
 const app = new Vue({
   ...App
